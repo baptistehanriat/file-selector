9ffc7a81be7e2e1c6dc048e8498bb6d602b50174
Baptiste HANRIAT <baptiste@kianava.de>
2021-12-21 17:50:14 +0100

    Finish navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Folder } from "@mui/icons-material";
 import Modal from "@mui/material/Modal";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -21,27 +22,34 @@ function App() {
   const [showModal, setShowModal] = useState(false);
 
   const dataUrl = "https://api-dev.reo.so/api/folderStructure";
-
   const [rootFolder, setRootFolder] = useState<Folder>();
   const [currentFolder, setCurrentFolder] = useState<Folder>();
+  const [history, setHistory] = useState<Folder[]>([]);
 
   const handleOpenModal = () => setShowModal(true);
   const handleCloseModal = () => {
     setShowModal(false);
     setCurrentFolder(rootFolder);
+    setHistory([]);
   };
 
   useEffect(() => {
-    axios.get(dataUrl).then((response) => {
-      setRootFolder(response.data);
-      setCurrentFolder(rootFolder);
-    });
+    axios
+      .get(dataUrl)
+      .then((response) => {
+        setRootFolder(response.data);
+        setCurrentFolder(response.data);
+      })
+      .catch((error) => {
+        // TODO: handle error
+        console.log(error);
+        return;
+      });
   }, []);
-  // TODO: add error state management for axios get
 
   function handleChange(selectedFolder: Folder) {
     setCurrentFolder(selectedFolder);
-    console.log(selectedFolder);
+    setHistory([...history, selectedFolder]); // doesn't update at the same time as outside function
   }
 
   return (
@@ -56,8 +64,20 @@ function App() {
       <ButtonPrimary onClick={handleOpenModal} label="Select Files" />
       <Modal open={showModal} onClose={handleCloseModal} hideBackdrop={true}>
         <View style={style}>
-          <h1>{currentFolder?.name}</h1>
-          <FolderView folder={currentFolder} onChange={handleChange} />
+          <h3
+            onClick={() => {
+              history.splice(-1); // cannot use Pop for tsx reasons
+              if (history.length > 0) {
+                setHistory(history);
+                setCurrentFolder(history[history.length - 1]);
+              } else {
+                setCurrentFolder(rootFolder);
+              }
+            }}
+          >
+            {currentFolder?.name}
+          </h3>
+          <FolderView folder={currentFolder!} onChange={handleChange} />
           <ButtonPrimary onClick={handleCloseModal} label="Close" />
         </View>
       </Modal>
@@ -66,26 +86,29 @@ function App() {
 }
 
 function FolderView(props: {
-  folder: Folder | undefined;
-  onChange: (selectedFolder: Folder) => void;
+  folder: Folder;
+  onChange: (selectedFolder: Folder, currentFolder: Folder) => void;
 }) {
   return (
     <View>
       {props.folder?.folders.map((folder) => {
         return (
-          <h1 onClick={() => props.onChange(folder)} key={folder.id}>
+          <h1
+            onClick={() => props.onChange(folder, props.folder)}
+            key={folder.id}
+          >
             {folder.name}
           </h1>
         );
       })}
       {props.folder?.files.map((file) => {
-        return <File key={file.id} {...file} />;
+        return <FileView key={file.id} {...file} />;
       })}
     </View>
   );
 }
 
-function File(file: File) {
+function FileView(file: File) {
   return (
     <View>
       <h1>{file.name}</h1>
